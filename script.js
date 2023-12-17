302193604794fe80f473159be6a0eae4fcc75bc8
Andrew Laudato <andrewlaudato@gmail.com>
2023-12-17 09:21:11 -0600

    x

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 document.getElementById('quiz-form').addEventListener('submit', function(event) {
     event.preventDefault();
     let results = [];
@@ -5,9 +6,12 @@ document.getElementById('quiz-form').addEventListener('submit', function(event) 
         let answer = document.getElementById('q' + i).value;
         results.push(answer);
     }
-    document.getElementById('result').textContent = 'Your answers: ' + results.join(', ');
+
+    const prompt = "You are a high school science teacher. The five questions from the Health Quiz app are: What is the benefit of exercise? Why do people take magnesium? What are the risks of being overweight? What is the benefit of protein? What is the benefit of taking a multivitamin? a student provided these answers to the above questions. give them a grade and a very short summary. The answers are "
+    
+
     var combinedResults = results.join(' ');
-    document.getElementById('displayResults').textContent = combinedResults;
+    document.getElementById('displayResults').textContent = prompt + combinedResults;
 
 
 });
