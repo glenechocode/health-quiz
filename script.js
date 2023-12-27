3e62b5041063daebe33c3c5f2aa4e9d867093fdc
Andrew Laudato <andrewlaudato@gmail.com>
2023-12-27 17:04:31 -0500

    cvcv

--- script.js
+++ script.js
@@ -15,33 +15,40 @@ document.getElementById('quiz-form').addEventListener('submit', function(event) 
 
 //call to Lamda API 
 
-// JavaScript to call the API and display results in the div
 
-// assuming you have a function to fetch from the API
-async function fetchData() {
+// Replace with your actual API Gateway URL
+const apiUrl = 'https://t66ueyich4.execute-api.us-east-1.amazonaws.com/default/OpenAI';
+
+// Function to call the API
+async function callLambdaAPI() {
   try {
-      const response = await fetch('https://uxxhhoaae4.execute-api.us-east-1.amazonaws.com/default/OpenAI', {
-          method: 'POST', // or 'GET' depending on your API
-          body: JSON.stringify({ data: 'wow' }),
-          headers: {
-              'Content-Type': 'application/json'
-              // Additional headers if required
-          }
-      });
-      const data = await response.json();
-      displayResults(data); // calling function to display the result
+    // Fetch data from the API
+    const response = await fetch(apiUrl, {
+      method: 'GET', // or 'POST' if your Lambda function expects a POST request
+      headers: {
+        'Content-Type': 'application/json',
+        // Additional headers can go here as needed
+      },
+    });
+
+    // Check if the request was successful
+    if (!response.ok) {
+      throw new Error(`Error! status: ${response.status}`);
+    }
+
+    // Parse the JSON response
+    const result = await response.json();
+    console.log('Response from Lambda:', result);
+
+    // Do something with the result
+    // Here you might update the DOM or return the result, etc.
   } catch (error) {
-      console.error('There was an error!', error);
+    console.error('Failed to fetch from the Lambda function:', error);
   }
 }
 
-function displayResults(apiResults) {
-  // Find the div with the id 'apiResults' and set its content
-  document.getElementById('apiResults').innerText = apiResults;
-}
-
-fetchData(); // call the function to fetch and display data
-
+// Call the function
+callLambdaAPI();
 
 
 //end call to Lamda API 
