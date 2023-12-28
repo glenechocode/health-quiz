4a7e6361c4ca97dccf9f856d57b23ed6dcc8690b
Andrew Laudato <andrewlaudato@gmail.com>
2023-12-28 12:24:15 -0500

    cc

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ document.getElementById('quiz-form').addEventListener('submit', function(event) 
   const prompt = "You are a high school science teacher. The five questions from the Health Quiz app are: What is the benefit of exercise? Why do people take magnesium? What are the risks of being overweight? What is the benefit of protein? What is the benefit of taking a multivitamin? a student provided these answers to the above questions. give them a grade and a very short summary. The answers are ";
   var combinedResults = results.join(' ');
 
-  console.log("Sending Results: ", combinedResults); // Log the results being sent
 
   // Call to Lambda API
   fetch('https://ydh2l6r29j.execute-api.us-east-1.amazonaws.com/default/OpenAI', {
@@ -19,6 +18,8 @@ document.getElementById('quiz-form').addEventListener('submit', function(event) 
           // Include any other headers your API requires
       },
       body: JSON.stringify({ prompt: prompt + combinedResults }),
+      console.log("Sending Results: ", combinedResults); // Log the results being sent
+
   })
   .then(response => {
       console.log("Raw Response: ", response); // Log the raw response
