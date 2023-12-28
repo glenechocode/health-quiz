cd18e808e33a8bec691859aa4525a9b6c18ecdea
Andrew Laudato <andrewlaudato@gmail.com>
2023-12-28 11:28:42 -0500

    cc

--- script.js
+++ script.js
@@ -9,29 +9,23 @@ document.getElementById('quiz-form').addEventListener('submit', function(event) 
   const prompt = "You are a high school science teacher. The five questions from the Health Quiz app are: What is the benefit of exercise? Why do people take magnesium? What are the risks of being overweight? What is the benefit of protein? What is the benefit of taking a multivitamin? a student provided these answers to the above questions. give them a grade and a very short summary. The answers are ";
   var combinedResults = results.join(' ');
 
-  // Constructing the full prompt
-  const fullPrompt = prompt + combinedResults;
-
-  // Define the API URL
-  const apiUrl = 'https://ydh2l6r29j.execute-api.us-east-1.amazonaws.com/default/OpenAI';
-
-  // Make the call to Lambda API using Fetch
-  fetch(apiUrl, {
-      method: 'POST', // or 'PUT' if your Lambda is set up to take PUT requests
+  // Call to Lambda API
+  fetch('https://ydh2l6r29j.execute-api.us-east-1.amazonaws.com/default/OpenAI', {
+      method: 'POST', // or 'PUT' if your API supports it
       headers: {
           'Content-Type': 'application/json',
+          // Include any other headers your API requires
       },
-      body: JSON.stringify({ text: fullPrompt }),
+      body: JSON.stringify({ prompt: prompt + combinedResults }),
   })
   .then(response => response.json())
   .then(data => {
-      // Assuming the Lambda returns a JSON object with a 'message' key
-      const apiReturn = data.message;
+      const apiReturn = data.body; // Adjust depending on the format of your response
       document.getElementById('displayResults').textContent = apiReturn;
   })
   .catch((error) => {
       console.error('Error:', error);
   });
 
+  //end call to Lambda API 
 });
-
