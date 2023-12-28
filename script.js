79fb0c7d281abf4db6b1fda06f8610d6dd216cee
Andrew Laudato <andrewlaudato@gmail.com>
2023-12-28 12:41:53 -0500

    cc

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ document.getElementById('quiz-form').addEventListener('submit', function(event) 
   const prompt = "You are a high school science teacher. The five questions from the Health Quiz app are: What is the benefit of exercise? Why do people take magnesium? What are the risks of being overweight? What is the benefit of protein? What is the benefit of taking a multivitamin? a student provided these answers to the above questions. give them a grade and a very short summary. The answers are ";
   var combinedResults = results.join(' ');
 
-
   // Call to Lambda API
   fetch('https://ydh2l6r29j.execute-api.us-east-1.amazonaws.com/default/OpenAI', {
       method: 'POST',
@@ -18,23 +17,16 @@ document.getElementById('quiz-form').addEventListener('submit', function(event) 
           // Include any other headers your API requires
       },
       body: JSON.stringify({ prompt: prompt + combinedResults }),
-      
   })
   .then(response => {
       console.log("Raw Response: ", response); // Log the raw response
       return response.json(); // Convert response to JSON
   })
-  
   .then(data => {
-    console.log("Data Received: ", data); // Log the data received
-    const apiReturn = data.message; // Access the message property of the response
-    document.getElementById('apiReturn').textContent = apiReturn;
-})
-
-
-
-
-
+      console.log("Data Received: ", data); // Log the data received
+      const apiReturn = data.message; // Access the message property of the response
+      document.getElementById('apiReturn').textContent = apiReturn;
+  })
   .catch((error) => {
       console.error('Error:', error);
   });
